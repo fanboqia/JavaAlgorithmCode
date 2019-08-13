e5959139b539530eb72568f9f16fec2e7cbc2370
fanboqia <boqianfan@gmail.com>
2019-08-13 09:59:14 -0400

    Submit RemoveDuplicates Two

--- src/main/java/com/lintcode/RemoveDuplicates.java
+++ src/main/java/com/lintcode/RemoveDuplicates.java
@@ -37,4 +37,35 @@ public class RemoveDuplicates {
         }
         return index+1;
     }
+
+    /*
+        Given a sorted array, remove the duplicates in place such that each element appear at most twice and return the new length.
+        If a number appears more than two times, then keep the number appears twice in array after remove.
+     * @param nums: An ineger array
+     * @return: An integer
+     */
+    public static int removeDuplicatesToTwice(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int i = 0;
+        int countDistinct = 0;
+        while(i < nums.length){
+            int twice = 0;
+            while(i < nums.length-1 && nums[i] == nums[i+1]){
+                i++;
+                twice++;
+            }
+            if(twice >= 1){
+                nums[countDistinct] = nums[i];
+                nums[countDistinct+1] = nums[i];
+                countDistinct+=2;
+            }else{
+                nums[countDistinct] = nums[i];
+                countDistinct++;
+            }
+            i++;
+        }
+        return countDistinct;
+    }
 }
--- src/test/java/com/lintcode/TestRemoveDuplicates.java
+++ src/test/java/com/lintcode/TestRemoveDuplicates.java
@@ -16,10 +16,18 @@ public class TestRemoveDuplicates {
         assertTrue( RemoveDuplicates.removeDuplicates(new int[]{1,1,2,2,3,10}) == 4);
     }
 
-    @Test
+    //@Test
     public void testPrintDuplicates(){
         int[] arr = new int[]{1,1,2,2,3,10,10};
         int size = RemoveDuplicates.removeDuplicates(arr);
         System.out.println(Arrays.toString(Arrays.copyOfRange(arr,0,size)));
     }
+
+    @Test
+    public void testRemoveDuplicatesToTwice(){
+        int[] arr = new int[]{1,1,2,2,2,3,10,10,10};
+        int size = RemoveDuplicates.removeDuplicatesToTwice(arr);
+        System.out.println(Arrays.toString(Arrays.copyOfRange(arr,0,size)));
+    }
+
 }
